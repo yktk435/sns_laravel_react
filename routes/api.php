24c0bc764d6414178092aff94a3bf83367ac221e
yktk435 <yokotsuka435@gmail.com>
2020-10-10 22:20:06 +0900

    apiルート確保とresourceでapiのチェック

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ひとまずテストのために
+Route::group(['middleware' => ['api']], function () {
+    Route::resource('/test', 'RestTestController');
+});
